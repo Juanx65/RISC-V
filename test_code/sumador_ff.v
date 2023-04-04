bd39eb502b5bb9a5f90995abde9cc43db0ad22cf
vyrkka <63375553+vyrkka@users.noreply.github.com>
2023-04-04 15:26:10 -0400

    Update sumador_ff.v

--- test_code/sumador_ff.v
+++ test_code/sumador_ff.v
@@ -16,9 +16,9 @@ module seq_adder(
     if (rst) begin
       sum <= 8'b0;
       carry <= 1'b0;
-      input_a_ff <= '0;
-      input_b_ff <= '0;
-      output_ff <= '0;
+      input_a_ff <= 0;
+      input_b_ff <= 0;
+      output_ff <= 0;
     end else begin
       input_a_ff <= input_a;
       input_b_ff <= input_b;
