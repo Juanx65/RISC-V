401961c6cdffab6c220f3427b5cb935ada2600b2
vyrkka <63375553+vyrkka@users.noreply.github.com>
2023-04-04 17:00:28 -0400

    Update logic_synthesis.tcl

--- logic_synthesis.tcl
+++ logic_synthesis.tcl
@@ -1,33 +1,51 @@
-#### run this command as: dc_shell -f config_tcl.tcl
+set_app_var target_library "sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_100C_1v95.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_100C_1v40.db"
+set_app_var link_library "* $target_library"
 
-# set sykwalk130 cell library
+# RTL Reading and Link
+analyze -format sverilog {test_code/adder_tree.v}
+elaborate adder_tree_fp
 
-set link_library [list sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_100C_1v65.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_100C_1v95.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_n40C_1v56.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_n40C_1v65.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_n40C_1v76.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_n40C_1v76.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_100C_1v40.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_100C_1v60.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_100C_1v60.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v28.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v35.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v40.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v44.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v76.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__tt_025C_1v80.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__tt_100C_1v80.db]
-set target_library [list sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_100C_1v65.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_100C_1v95.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_n40C_1v56.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_n40C_1v65.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_n40C_1v76.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_n40C_1v76.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_100C_1v40.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_100C_1v60.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_100C_1v60.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v28.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v35.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v40.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v44.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ss_n40C_1v76.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__tt_025C_1v80.db sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__tt_100C_1v80.db]
-set symbol_library ""
+link
 
-#read, analyze and elaborate the rtl code
-read_file -format sverilog {test_code/sumador.sv}
-analyze -library WORK -format verilog {test_code/sumador.sv}
-elaborate adder1bit -architecture verilog -library WORK
-analyze {}
+# Constrains Setup
+set clk_val 10
 
-#provide constraints
-#create_clock -name "clk" -period 10 -waveform { 5 10 }
-set_max_area 8000
-set_max_fanout 8 adder1bit
-set_max_transition 5 adder1bit
+create_clock -period $clk_val [get_ports clk*] -name clk
+set_clock_uncertainty -setup [expr $clk_val*0.1] [get_clocks clk]
+set_clock_transition -max [expr $clk_val*0.2] [get_clocks clk]
+set_clock_latency -source -max [expr $clk_val*0.05] [get_clocks clk]
 
-#compile the rtl code
+set_input_delay -max [expr $clk_val*0.4] -clock clk [get_ports [remove_from_collection [all_inputs] clk]]
+set_output_delay -max [expr $clk_val*0.5] -clock clk [get_ports [all_outputs]]
+
+set_load -max 0.04 [get_ports [all_outputs]]
+set_input_transition -min [expr $clk_val*0.01] [get_ports [all_inputs]]
+set_input_transition -max [expr $clk_val*0.1] [get_ports [all_inputs]]
+
+# Pre-compile Reports
+report_clock > ./report/report_clock.rpt
+analyze_datapath_extraction > ./report/analyze_datapath_extraction.rpt                                                       
+
+# Compile/Synthesis
+# compile_ultra -no_autoungroup
+
 compile -exact_map
 
-#report generation
-file mkdir report
-write -hierarchy -format verilog -output report/sumador_gtlvl.v
-write_sdc -nosplit -version 2.0 report/sumador.sdc
-report_area -hierarchy > report/sumador.area
-report_timing > report/sumador.timing
-report_power > report/sumador.power
+# Post-compile Reports
+analyze_datapath > report/analyze_datapath.rpt
+report_resources -hierarchy > report/report_resources.rpt
+report_qor > report/report_qor.rpt
+report_timing > report/report_timing.rpt
+report_area > report/report_area.rpt
+report_power > report/report_power.rpt
 
-#start GUI
+# Save Design
+write_file -format ddc -hierarchy -out report/test1.ddc
+
+write_file -format verilog -hierarchy -out report/test11.v
+write_sdc report/lab1.sdc
+
 gui_start
+
+# Exit
+# exit
