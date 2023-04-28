05242ef1c72c847b7d1067f246dc294be85ffcc7
juanx65 <juan.aguileraca@sansano.usm.cl>
2023-04-28 17:08:57 -0400

    tenemos routing, falta vdd y vss

--- icc2_test.tcl
+++ icc2_test.tcl
@@ -1,73 +1,66 @@
-## call out libraries
+## Call out libraries
 set_app_var target_library "sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_100C_1v95.db"
 set_app_var link_library "* $target_library"
-
 ## change design here
-#sh rm -r /home/usuario12/RISC-V/icc2out/uniciclo_LIB
-
-## create milkway enviroment
-
-## change desigm here
+#sh rm -r /home/usuario12/RISC-V/uniciclo_LIB
+#------------------------------------------
+#  Create milkway enviroment
+# -----------------------------------------
 create_lib -tech "sky130_fd_sc_hd/skywater130_fd_sc_hd.tf" -ref_libs "sky130_fd_sc_hd/ndm/sky130_fd_sc_hd.ndm" uniciclo_LIB
-
-## 
 open_lib uniciclo_LIB
-
-##
 read_parasitic_tech  -tlup "sky130_fd_sc_hd/skywater130.nominal.tluplus" -layermap "sky130_fd_sc_hd/skywater130.mw2itf.map" -name nomTLU
 
-## 
+#------------------------------------------
+#  Design
+# -----------------------------------------
 read_verilog "report/alu.v"
-
-##
 read_sdc "report/alu.sdc"
 
-##
+#------------------------------------------
+#  Placement
+# -----------------------------------------
 initialize_floorplan  -core_utilization 0.7  -core_shape R  -orientation N  -core_side_ratio {1.5 1.0} -core_offset {0.5}  -flip_first_row true  -coincident_boundary true
 
-#
-create_placement -floorplan
-## ring
-create_io_ring -name outer_ring -corner_height 0.5
-
-create_net -power VDD2
-create_net -ground VSS2
-create_power_domain PD1
-#create_power_switch SW1 -input_supply_port {vin VDD2} -domain {PD1}
-connect_pg_net -automatic
-
-set_power_io_constraints -reference_cell {VDD2 VSS2}
-
-place_io
-
-#ring prometedor
+#------------------------------------------
+#  Power Rings
+# -----------------------------------------
 
 connect_pg_net -automatic -all_blocks
 
-create_pg_ring_pattern ring_pattern -horizontal_layer M1 \
+create_pg_ring_pattern ring_pattern -horizontal_layer met1 \
    -horizontal_width {1.5} -horizontal_spacing {0.5} \
-   -vertical_layer M2 -vertical_width {1.5} -vertical_spacing {0.5}
+   -vertical_layer met2 -vertical_width {1.5} -vertical_spacing {0.5}
 set_pg_strategy core_ring \
    -pattern {{name: ring_pattern} \
    {nets: {VDD VSS}} {offset: {0.5 0.5}}} -core
 compile_pg -strategies core_ring
 
-## or guides: ver man pages de icc2
-#create_io_guide -name "io_guide1" -line {{100 100} 100} -side left -pad_cells { pad1 pad2 }
+## Falta mesh grid aca https://github.com/SkillSurf/systemverilog/blob/91b4842a7377734b780b910e659ddff0a21ee7d0/asic_flow/scripts/icc2/icc2.tcl
 
-## power constraints: # no funciona porque dice que VDD_NS no existe
-#set_power_io_constraints -io_guide_object [get_io_guides *] -ratio {{3 VDD_NS} {7 VSS_NS}} -reference_cell {VDD_NS VSS_NS}
+#------------------------------------------
+#  Pin I/O - MODIFY the pins as required
+# -----------------------------------------
+place_pins -self -ports {VDD VSS SrcA SrcB ALUControl ALUResult Zero}
+
+
+#------------------------------------------
+#  Placement
+# -----------------------------------------
+create_placement -floorplan -timing_driven
+
+# -----------------------------------------
+#  Route
+# -----------------------------------------
+
+#clock_opt
+route_auto -max_detail_route_iterations 5
+#save_block $library_name:$top_module
+#write_gds -hier all ${top_module}.gds
 
 ##
-#create_routing_rule rdlrule -widths {met2 2} -spacings {met2 1}
-
-## place pins
-#place_pins -self
-
-## floorplan: actualmente fallando en uniciclo
-#create_placement -floorplan
+#compile
+#save_lib -all
 
 ### run with: icc2_shell -gui -f icc2_test.tcl
-
 ## exit shell
 # exit
