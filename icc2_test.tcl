0aad63367631af8c1b2e9d5b23f45fae5f467348
juanx65 <juan.aguileraca@sansano.usm.cl>
2023-04-28 16:04:55 -0400

    fracasando en floorplan

--- icc2_test.tcl
+++ icc2_test.tcl
@@ -22,8 +22,39 @@ read_verilog "report/alu.v"
 ##
 read_sdc "report/alu.sdc"
 
+##
 initialize_floorplan  -core_utilization 0.7  -core_shape R  -orientation N  -core_side_ratio {1.5 1.0} -core_offset {0.5}  -flip_first_row true  -coincident_boundary true
-create_placement -floorplan 
+
+#
+create_placement -floorplan
+## ring
+create_io_ring -name outer_ring -corner_height 0.5
+
+create_net -power VDD2
+create_net -ground VSS2
+create_power_domain PD1
+create_power_switch SW1 -input_supply_port {vin VDD2} -domain {PD1}
+connect_pg_net -automatic
+
+set_power_io_constraints -reference_cell {VDD2 VSS2}
+
+place_io
+
+## or guides: ver man pages de icc2
+#create_io_guide -name "io_guide1" -line {{100 100} 100} -side left -pad_cells { pad1 pad2 }
+
+## power constraints: # no funciona porque dice que VDD_NS no existe
+#set_power_io_constraints -io_guide_object [get_io_guides *] -ratio {{3 VDD_NS} {7 VSS_NS}} -reference_cell {VDD_NS VSS_NS}
+
+##
+#create_routing_rule rdlrule -widths {met2 2} -spacings {met2 1}
+
+## place pins
+#place_pins -self
+
+## floorplan: actualmente fallando en uniciclo
+#create_placement -floorplan
+
 ### run with: icc2_shell -gui -f icc2_test.tcl
 
 ## exit shell
--- logic_synthesis.tcl
+++ logic_synthesis.tcl
@@ -3,26 +3,26 @@ set_app_var target_library "sky130_fd_sc_hd/db_nldm/sky130_fd_sc_hd__ff_100C_1v9
 set_app_var link_library "* $target_library"
 
 ## RTL Reading and Link
-#analyze -format sverilog {Uniciclo/top.sv Uniciclo/adder.sv Uniciclo/alu.sv Uniciclo/aludec.sv Uniciclo/controller.sv Uniciclo/datapath.sv Uniciclo/dmem.sv Uniciclo/extend.sv Uniciclo/flopr.sv Uniciclo/flopenr.sv Uniciclo/imem.sv Uniciclo/maindec.sv Uniciclo/mux2.sv Uniciclo/mux3.sv Uniciclo/regfile.sv Uniciclo/riscvsingle.sv}
-analyze -format sverilog test_code/alu_test.sv
-elaborate alu
+analyze -format sverilog {Uniciclo/top.sv Uniciclo/adder.sv Uniciclo/alu.sv Uniciclo/aludec.sv Uniciclo/controller.sv Uniciclo/datapath.sv Uniciclo/dmem.sv Uniciclo/extend.sv Uniciclo/flopr.sv Uniciclo/flopenr.sv Uniciclo/imem.sv Uniciclo/maindec.sv Uniciclo/mux2.sv Uniciclo/mux3.sv Uniciclo/regfile.sv Uniciclo/riscvsingle.sv}
+#analyze -format sverilog test_code/alu_test.sv
+elaborate top
 
 link
 
 ## Constrains Setup
-# set clk_val 10
+set clk_val 10
 
-# create_clock -period $clk_val [get_ports clk*] -name clk
-# set_clock_uncertainty -setup [expr $clk_val*0.1] [get_clocks clk]
-# set_clock_transition -max [expr $clk_val*0.2] [get_clocks clk]
-# set_clock_latency -source -max [expr $clk_val*0.05] [get_clocks clk]
+create_clock -period $clk_val [get_ports clk*] -name clk
+set_clock_uncertainty -setup [expr $clk_val*0.1] [get_clocks clk]
+set_clock_transition -max [expr $clk_val*0.2] [get_clocks clk]
+set_clock_latency -source -max [expr $clk_val*0.05] [get_clocks clk]
 
-# set_input_delay -max [expr $clk_val*0.4] -clock clk [get_ports [remove_from_collection [all_inputs] clk]]
-# set_output_delay -max [expr $clk_val*0.5] -clock clk [get_ports [all_outputs]]
+set_input_delay -max [expr $clk_val*0.4] -clock clk [get_ports [remove_from_collection [all_inputs] clk]]
+set_output_delay -max [expr $clk_val*0.5] -clock clk [get_ports [all_outputs]]
 
-# set_load -max 0.04 [get_ports [all_outputs]]
-# set_input_transition -min [expr $clk_val*0.01] [get_ports [all_inputs]]
-# set_input_transition -max [expr $clk_val*0.1] [get_ports [all_inputs]]
+set_load -max 0.04 [get_ports [all_outputs]]
+set_input_transition -min [expr $clk_val*0.01] [get_ports [all_inputs]]
+set_input_transition -max [expr $clk_val*0.1] [get_ports [all_inputs]]
 
 ## Pre-compile Reports
 # Nombre del directorio que deseas crear
@@ -39,8 +39,8 @@ if {[file exists $directorio]} {
 # analyze_datapath_extraction > report/analyze_datapath_extraction.rpt                                                       
 
 ## Compile/Synthesis
-compile_ultra -no_autoungroup
-#compile -exact_map
+#compile_ultra -no_autoungroup
+compile -exact_map
 
 ## Post-compile Reports
 # analyze_datapath > report/analyze_datapath.rpt
@@ -51,10 +51,10 @@ compile_ultra -no_autoungroup
 # report_power > report/report_power.rpt
 
 ## Save Design
-write_file -format ddc -hierarchy -out report/alu.ddc
+write_file -format ddc -hierarchy -out report/uniciclo.ddc
 
-write_file -format verilog -hierarchy -out report/alu.v
-write_sdc report/alu.sdc
+write_file -format verilog -hierarchy -out report/uniciclo.v
+write_sdc report/uniciclo.sdc
 
 ## Start GUI
 #gui_start
